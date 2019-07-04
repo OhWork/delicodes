7cc0447b4c99196495fb899b5560743654c6e4cc
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-07-04 21:40:33 +0700

    โอ้โน๊วว

--- footer.php
+++ footer.php
@@ -3,55 +3,6 @@
 		<div class="col-12 bgf">
 			<div class="row">
 				<div class="col-5">
-					<div class="row">
-						<div class="col-12 mt-3">
-							<div class="row">
-								<div class="col-3"></div>
-								<div class="col-6 torange fs3">ส่งข้อความ</div>
-								<div class="col-3"></div>
-							</div>
-						</div>
-						<div class="col-12">
-							<div class="row">
-								<div class="col-3"></div>
-								<div class="col-6">
-									<div class="row">
-										<div class="torange ml-3">ถึง  Deli</div>
-										<div class="twhite">codes</div>
-									</div>
-								</div>
-								<div class="col-3"></div>
-							</div>
-						</div>
-						<div class="col-12 mt-2">
-							<div class="row">
-								<div class="col-3 twhite text-right pt-1"><p>Name</p></div>
-								<div class="col-6 torange"><input class="w-100 br" placeholder="กำลังปรับปรุง"/></div>
-								<div class="col-3"></div>
-							</div>
-						</div>
-						<div class="col-12">
-							<div class="row">
-								<div class="col-3 twhite text-right pt-1"><p>TEL</p></div>
-								<div class="col-6 torange"><input class="w-100 br" placeholder="กำลังปรับปรุง"/></div>
-								<div class="col-3"></div>
-							</div>
-						</div>
-						<div class="col-12">
-							<div class="row">
-								<div class="col-3 twhite text-right pt-1"><p>E-mail</p></div>
-								<div class="col-6 torange"><input class="w-100 br" placeholder="กำลังปรับปรุง"/></div>
-								<div class="col-3"></div>
-							</div>
-						</div>
-						<div class="col-12 mb-3">
-							<div class="row">
-								<div class="col-4"></div>
-								<div class="col-4" align="center"><button type="button" class="btn bgorance twhite">Submit</button></div>
-								<div class="col-4"></div>
-							</div>
-						</div>
-					</div>
 				</div>
 				<div class="col-2"></div>
 				<div class="col-5">
